03da2e8e1a16dec8735337741d849a95e162a857
August-Z <402279076@qq.com>
2018-12-03 15:10:31 +0800

    Project init;

---
+++ app/src/main/java/com/z/august/www/xunji_demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.z.august.www.xunji_demo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
